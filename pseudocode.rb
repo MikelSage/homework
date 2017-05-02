39f46338c8366aafc5bd8bc931ec3d9e6629444c
MikelSage <michael.sagapolutele@yahoo.com>
2017-05-02 09:09:54 -0600

    initial commit

---
+++ pseudocode.rb
@@ -0,0 +1,25 @@
+# create variable to tell loop to end, boolean
+# create variable to keep track if "GOODBYE!" is said, boolean
+# output introductory phrase
+
+# get user input
+
+#start loop, ends when quit variable is true
+## check if input is empty
+### output "HELLO?!"
+
+## check if input has lowercase letters
+### output "I AM HAVING A HARD TIME HEARING YOU"
+
+## check if input is all uppercase
+### output "NO THIS IS NOT A PET STORE"
+
+## check if input is "GOODBYE!" and GOODBYE! variable is false
+### output "ANYTHING ELSE I CAN HELP WITH?"
+## check if input is "GOODBYE!" and GOODBYE! variable is true
+### output "THANK YOU FOR CALLING!"
+### set loop variable to true
+
+# end loop
+
+# output thank you for calling message
